977f2f0bf48e0ac1dfba54444f7c8189dfdba6d4
Dinesh Polaki <dineshpolaki@gmail.com>
2022-07-15 08:19:22 +0530

    End of section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { SharedModule } from './_modules/shared.module';
 import { ToastrModule } from 'ngx-toastr';
@@ -21,6 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 
 @NgModule({
@@ -35,7 +37,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +49,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     SharedModule 
   ],
   providers: [
-    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor, multi:true}
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor, multi:true},
+    {provide:HTTP_INTERCEPTORS,useClass:JwtInterceptor, multi:true}
   ],
   bootstrap: [AppComponent]
 })
